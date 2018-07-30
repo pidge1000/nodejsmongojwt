b2b11a7cfb7a58d7aeb258189101c995c03ee83b
“Prashant <prashant.22910@gmail.com>
2018-07-30 19:52:16 +0530

    WIP

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,33 +2,65 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 const User = require('../models/model/user.model')
 
 router.post('/signup', function(req, res) {
 
    bcrypt.hash(req.body.password, 10, function(err, hash) {
-
       if(err) {
          return res.status(500).json({
-            error: err
+            message: "Failed to process request"
          });
-      }
-      else {
+      } else {
          const user = new User({
             email: req.body.email,
             password: hash    
          });
-         user.save().then(function(result) {
-            res.status(200).json({
-               success: 'New user has been created'
-            });
-         }).catch(error => {
-            res.status(500).json({
-               error: err
-            });
-         });
+         user.save(function(err, result) {
+            if(err) {
+               res.status(500).json({
+                  message: "Failed to Save user info"
+               });
+            }
+
+            if(result) {
+               res.status(200).json({
+                  message: 'New user has been created',
+               });
+            }
+            
+         })
       }
-   });
-});
+   })
+
+})
+
+
+router.post('/signin', function(req, res) {
+
+   User.findOne({email: req.body.email}, function(err, user) {
+      if (err) {
+         return res.status(500).json({
+            message: "Failed to process request"
+         })
+      }
+
+      if (!user) {
+         return res.status(404).json({
+            message: "No user found"
+         })
+      }
+
+
+      const passwordIsValid = bcrypt.compare(req.body.password, user.password)
+      if (!passwordIsValid) return res.status(401).json({ message: "Password does not match" })
+
+      const jwtToken = jwt.sign({id: user._id}, 'JWT', {expiresIn: '2h'})
+      res.status(200).json({ auth: true, token: jwtToken });
+
+   })
+
+})
 
 module.exports = router;
