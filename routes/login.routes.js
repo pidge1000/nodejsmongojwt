735661ea6d47180481adf14fd1028330da3ab1ad
“Prashant <prashant.22910@gmail.com>
2018-09-06 19:15:08 +0530

    WIP

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const config = require('../config')
