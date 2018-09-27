c3231ad52416acd5cc221efcbb8856e0c0217cb8
“Prashant <prashant.22910@gmail.com>
2018-09-27 16:49:25 +0530

    WIP

--- models/MysqlConnection.js
+++ models/MysqlConnection.js
@@ -4,7 +4,12 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
 	host: process.env.MysqlHOST || 'localhost',
 	user: process.env.MysqlUser || 'root',
-	password: process.env.MysqlPassword || 'root'
+	password: process.env.MysqlPassword || 'root',
+	database: 'EdTech'
 });
+
+connection.connect(function(error) {
+	if(error) console.log('Error connecting db:', error.message)
+})
 
 exports.connection = connection
