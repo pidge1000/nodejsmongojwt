22b906e358933fd78ed00c41ce47f995607fc9f3
Prashant <prashant.22910@gmail.com>
2018-09-30 10:06:36 +0530

    WIP

--- auth/verifyMigration.js
+++ auth/verifyMigration.js
@@ -1,7 +1,7 @@
 function verifyMigration(req, res, next) {
 
     if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-        if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[1] === 'pidge') {
+        if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[1] === 'pidgeljlksdjkdsljlkdjgfdlkjglfdkjgfdlkj') {
             next();
         } else {
         	return res.status(401).json({ status: 401, message: 'No Authorization'})
