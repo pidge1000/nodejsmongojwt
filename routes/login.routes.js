c5251dc34bbfbc412c3870b53cfe5525a1edc576
Prashant <prashant.22910@gmail.com>
2018-10-01 00:36:07 +0530

    WIP

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -7,7 +7,7 @@ const User = require('../models/model/User.model')
 
 
 router.post('/signup', (req, res) => {
-
+    /*
    bcrypt.hash(req.body.password, config.jwtSalt, (err, jwtHash) => {
       if(err) {
          return res.status(500).json({ status: 500, message: "Failed to process requesttttt" })
@@ -35,28 +35,33 @@ router.post('/signup', (req, res) => {
          })
       }
    })
-
+    */
 })
 
 
 router.post('/signin', function(req, res) {
 
-   User.findOne({ email: req.body.email })
-   .exec()
-   .then((user) => {
-      if (user) {
-         const passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
-         if (!passwordIsValid) return res.status(401).json({ status: 401, message: "Password does not match" })
-         
-         const jwtToken = jwt.sign({id: user._id}, config.jwtSecret, {expiresIn: config.jwtExpiresIn})
-         return res.status(200).json({ status: 200, token: jwtToken, message: "Signin successfully" })
-      } else {
-         return res.status(404).json({ status: 404, message: "No user found" })
-      }
-   })
-   .catch((err) => {
-      return res.status(500).json({status: 500, message: "Failed to process request" })
-   })
+    let globalUser;
+    User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+        if (user) {
+            globalUser = user
+            return bcrypt.compare(req.body.password, user.password)
+        } else {
+            return res.status(404).json({ status: 404, message: "No user found" })
+        }
+    })
+    .then((passwordIsValid) => {
+        if(!passwordIsValid)
+            return res.status(401).json({ status: 401, message: "Password does not match" })
+
+        const jwtToken = jwt.sign({id: globalUser._id, email: globalUser.email, type: globalUser.type, status: globalUser.status}, config.jwtSecret, {expiresIn: config.jwtExpiresIn})
+        return res.status(200).json({ status: 200, token: jwtToken, message: "Signin successfully" })
+    })
+    .catch((err) => {
+        return res.status(500).json({status: 500, message: "Failed to process request" })
+    })
 
 })
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,14 +2,15 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/model/User.model')
 
-router.post('/me', (req, res) => {
+router.get('/me', (req, res) => {
 
-   User.findById(req.id, (err, user) => {
-      if (err) return res.status(500).json({ status: 500, message: 'There was a problem finding the user' })
-      if (!user) return res.status(404).json({ status: 404, message: 'No user found'})
-      	
-      res.status(200).json({ status: 200, message: 'Success', user: user});
-   });
+   	User.findById(req.id, (err, user) => {
+      	if (err) return res.status(500).json({ status: 500, message: 'There was a problem finding the user' })
+      	if (!user) return res.status(404).json({ status: 404, message: 'No user found'})
+    	
+    	user.password = undefined  
+      	res.status(200).json({ status: 200, message: 'Success', user: user});
+   	});
 
 })
 
